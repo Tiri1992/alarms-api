4d3ade88228e48bed33f7f14fb8e4d0cd1a47506
Tiri <tirigeorgiou@gmail.com>
2022-07-31 15:30:54 +0100

    changing alarms field name from hour to time
    changed Alarms field hour -> time
    
    changed Alarms field hour -> time

--- app/schemas/alarms.py
+++ app/schemas/alarms.py
@@ -1,6 +1,6 @@
 """Response models for returned data from server."""
 from pydantic import BaseModel, Field
-from datetime import datetime
+from datetime import datetime, time
 from typing import Union
 
 
@@ -27,8 +27,7 @@ class Alarm(AlarmBase):
     id: int
     day_of_week: int = Field(..., ge=1, le=7,
                              description="Day of week, Monday=1, Sunday=7.")
-    hour: int = Field(..., ge=1, le=24,
-                      description="Hour within 24 hour clock.")
+    time: time = Field(..., description="Hour within 24 hour clock.")
     created_at: datetime
     updated_at: Union[datetime, None]
 
